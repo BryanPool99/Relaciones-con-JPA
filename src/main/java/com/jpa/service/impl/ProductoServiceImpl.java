d01f92786d2eabc5c104ba5c67ccb422f33b92fc
Bryan <bryanpool991@hotmail.com>
2024-03-11 12:07:34 -0500

    corregido errores de nomenclatura

--- src/main/java/com/jpa/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/jpa/service/impl/ProductoServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProductoServiceImp implements IProductoService {
+public class ProductoServiceImpl implements IProductoService {
     //inyeccion
     @Autowired
     private IProductoDAO productoDAO;
